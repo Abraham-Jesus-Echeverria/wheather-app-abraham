e54d1d2eacc65722e656b0d57c65adb22bdf1291
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2023-03-02 17:45:46 -0600

    eliminando importacion weatherApp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 // import bootstrap from "bootstrap"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"; 
-import "./assets/images/weatherApp.webp"; 
 
 const root = ReactDOM.createRoot(document.getElementById("App")); 
 root.render(
